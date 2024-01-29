3a9e8bc52f9367ac5eeeb64e6a840e8caa2797c9
SimonTheoret <simonteoret@hotmail.com>
2024-01-29 16:06:18 -0500

    modified wrapping for code

--- dot_config/nvim/lua/simon/autocmd.lua
+++ dot_config/nvim/lua/simon/autocmd.lua
@@ -1,44 +1,28 @@
 -- List of autocommands
 
 
+-- remap for neorg + textwidht, foldlevel, conceallevel
 vim.api.nvim_create_autocmd({ "BufRead" }, {
     pattern = { "*.norg" },
     callback = function(_)
         vim.api.nvim_buf_set_keymap(0, "n", "<tab>", "za", { desc = "Toggle fold" }) -- cute remap for neorg
         vim.bo.textwidth = 80
+        vim.wo.foldlevel = 99
+        vim.wo.conceallevel = 2
     end
 })
 
+-- markdown files set textwidth, conceallevel
 vim.api.nvim_create_autocmd({ "BufRead" }, {
     pattern = { "*.markdown", "*.md" },
     callback = function(_)
         vim.bo.textwidth = 80
-    end
-})
-vim.api.nvim_create_autocmd({"BufRead"}, {
-    pattern = {"*.norg"},
-    callback = function(_)
-        vim.wo.foldlevel = 99
         vim.wo.conceallevel = 2
     end
-
 })
---
--- Wraps lines of more then 80 characters in .norg, .md  files
--- vim.api.nvim_create_autocmd({"BufRead"}, {
---     pattern = { "*.norg", "*.md"  },
---     callback = function(_)
---         local pattern  = { "norg", "md"}
---         for ft in pattern do
---             if vim.bo.filetype == ft then
---                 vim.opt.textwidth = 80
---                 print("in a norg/md file")
---             else
---                 vim.opt.textwidth = 0
---                 print("not in a norg/ md file")
---             end
---         end
---     end })
+
+
+-- Add sheban in shell scripts
 vim.api.nvim_create_autocmd({ "BufNewFile" }, {
     pattern = { "*.sh" },
     callback = function(_)
