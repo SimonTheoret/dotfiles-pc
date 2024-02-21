5d5fcdb32cb16c67a36b5062cf4d9c0e8be2ea86
SimonTheoret <simonteoret@hotmail.com>
2024-02-21 15:36:28 -0500

    modified colors slightly and Oil

---
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+    'stevearc/oil.nvim',
+    config = true,
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+        "<leader>oo",
+        "<CMD>Oil<CR>",
+        desc = "Open oil"
+    },
+}
