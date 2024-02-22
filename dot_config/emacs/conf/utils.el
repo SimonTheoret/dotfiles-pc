28ba6d255f57ad58e73ed19a7fb856301b05a499
Simon Theoret <simonteoret@hotmail.com>
2024-02-22 15:10:30 -0500

    updated emacs conf

--- dot_config/emacs/conf/utils.el
+++ dot_config/emacs/conf/utils.el
@@ -1,18 +1,20 @@
 ;; -*- lexical-binding: t -*- 
 
-(defun utils-update-cm-emacs ()
-  "Update the chezmoi emacs files. It runs the following commands:
-`chezmoi add user-emacs-directory/*.el --force`
-`chezmoi add user-emacs-directory/conf/*.el --force`"
-  (interactive)
-  (let ((first-cmd-string
-         (concat
-          "chezmoi add " user-emacs-directory "*.el" " --force"))
-        (second-cmd-string
-         (concat
-          "chezmoi add "
-          user-emacs-directory
-          "conf/*.el"
-          " --force")))
-    (shell-command first-cmd-string nil)
-    (shell-command second-cmd-string nil)))
+;; (defun utils-update-cm-emacs ()
+;;   "Update the chezmoi emacs files. It runs the following commands: -
+;; `chezmoi add user-emacs-directory/*.el --force` -`chezmoi add
+;; user-emacs-directory/conf/*.el --force`"
+;;   (interactive)
+;;   (let ((first-cmd-string
+;;          (concat
+;;           "chezmoi add " user-emacs-directory "*.el" " --force"))
+;;         (second-cmd-string
+;;          (concat
+;;           "chezmoi add "
+;;           user-emacs-directory
+;;           "conf/*.el"
+;;           " --force")))
+;;     (shell-command first-cmd-string nil)
+;;     (shell-command second-cmd-string nil)
+;;     ))
+
